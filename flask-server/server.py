8deed16bad4ed1b8fc594180df976d6a901a9544
jacobmarxJHU <jmarx14@jh.edu>
2023-10-09 17:44:09 -0400

    initial push with established connection from FE to BE.  Tutorial from https://www.youtube.com/watch?v=7LNl2JlZKHA.  Also added .gitignore file which ignores venv. Use requirements.txt to establish baseline venv

---
+++ flask-server/server.py
@@ -0,0 +1,11 @@
+from flask import Flask
+import psycopg2
+
+app = Flask(__name__)
+
+@app.route("/members")
+def members():
+    return {"members": ["Member1", "Member2", "Member3"]}
+
+if __name__ == "__main__":
+    app.run(debug=True)

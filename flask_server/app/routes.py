dd5b1a7ce30c76afe49095c6cceadbb54ec8957e
Nicolas Miller <njmiller1208@gmail.com>
2023-10-21 14:23:49 -0400

    Modified Users model to include field for username. Added routes for user POST and username GET. Migrated and updated database to include new Users model.

--- flask_server/app/routes.py
+++ flask_server/app/routes.py
@@ -1,4 +1,6 @@
-from app import app
+from flask import request, jsonify
+from app import app, db
+from app.models import Users
 
 @app.route('/')
 @app.route('/index')
@@ -7,7 +9,33 @@ def index():
 
 @app.route('/members')
 def members():
-    return {"members": ["Member1", "Member2", "Member3"]}
+    return jsonify({"members": ["Member1", "Member2", "Member3"]})
+
+# Adds a new user and returns unique ID assigned to user
+@app.route('/user', methods=['POST'])
+def add_user():
+    data = request.json
+    username = data.get('username')
+
+    if not username:
+        return jsonify({"error": "Username not provided"}), 400
+
+    user = Users(username=username)
+    db.session.add(user)
+    db.session.commit()
+
+    return jsonify({'id': user.id, 'username': user.username}), 201
+
+@app.route('/user/<username>', methods=['GET'])
+def get(username):
+    user = Users.query.filter_by(username=username).first()
+
+    if not user:
+        return jsonify({'error': 'User not found.'}), 404
+
+    return jsonify({'id': user.id, 'username': user.username})
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+

cf2c916564ca089fe322dd22aa5ef4fa964a5539
nicolasnotcage <njmiller1208@gmail.com>
2023-11-09 13:20:26 -0500

    Adding migration files from database update.

---
+++ flask_server/migrations/versions/e880aece3143_added_hand_and_card_tables.py
@@ -0,0 +1,24 @@
+"""Added hand and card tables
+
+Revision ID: e880aece3143
+Revises: 2105de95ddfb
+Create Date: 2023-11-09 10:26:59.778724
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e880aece3143'
+down_revision = '2105de95ddfb'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass

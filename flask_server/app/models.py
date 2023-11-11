db773bfbd682a1637ead5dad7846d24679a815c6
jacobmarxJHU <jmarx14@jh.edu>
2023-11-10 21:11:17 -0500

    commiting new models.py and alembic migration

--- flask_server/app/models.py
+++ flask_server/app/models.py
@@ -4,16 +4,49 @@ in the schema with primary and foreign keys. Please tweak as needed.
 """
 
 from app import db
+from sqlalchemy import Integer, ForeignKey, String, Column, Boolean
+
+# TODO: Format with SQLAlchemy ORM format (decalarative base and mapped)
+# TODO: Add repr methods to each class
 
 
-class Users(db.Model):
-    __tablename__ = 'Users'
+# Characters Table
+class Character(db.Model):
+    __tablename__ = 'Characters'
     __table_args__ = {'schema': 'cs'}
 
-    id = db.Column(db.Integer, primary_key=True)
-    sessionInfo = db.Column(db.String)
-    gameCode = db.Column(db.String)
-    username = db.Column(db.String, unique=True, nullable=False)
+    id = Column(Integer, primary_key=True)
+    character = Column(String(20), nullable=False)
+
+
+# Cards Table
+class Cards(db.Model):
+    # TODO: Add method to get the associated character, location, or weapon string
+
+    __tablename__ = 'Cards'
+    __table_args__ = {'schema': 'cs'}
+
+    id = Column(Integer, primary_key=True)
+    locationId = Column(Integer, ForeignKey('cs.Locations.id'))
+    characterId = Column(Integer, ForeignKey('cs.Characters.id'))
+    weaponId = Column(Integer, ForeignKey('cs.Weapons.id'))
+
+
+# Games Table
+class Game(db.Model):
+    # TODO: add max player constarint to playerCount (set to 6)
+    # TODO: add min player constraint to playerCount (set to 0)
+    # TODO: add default gameCode (random 6 character string)
+    # TODO: make game status not nullable
+    # TODO: add default value for gameStatus
+
+    __tablename__ = 'Games'
+    __table_args__ = {'schema': 'cs'}
+
+    id = Column(Integer, primary_key=True)
+    gameStatus = Column(Integer, ForeignKey('cs.GameStatus.id'))
+    playerCount = Column(Integer, default=0, nullable=False)
+    gameCode = Column(String(6), nullable=False)
 
 
 # GameStatus Table
@@ -21,153 +54,170 @@ class GameStatus(db.Model):
     __tablename__ = 'GameStatus'
     __table_args__ = {'schema': 'cs'}
 
-    id = db.Column(db.Integer, primary_key=True)
-    status = db.Column(db.String)
-
-
-# ActivePlayers Table
-class ActivePlayers(db.Model):
-    __tablename__ = 'ActivePlayers'
-    __table_args__ = {'schema': 'cs'}
-
-    id = db.Column(db.Integer, primary_key=True)
-    playerName = db.Column(db.String)
-    playerCode = db.Column(db.String)
-
-
-# ActiveGames Table
-class ActiveGames(db.Model):
-    __tablename__ = 'ActiveGames'
-    __table_args__ = {'schema': 'cs'}
-
-    id = db.Column(db.Integer, primary_key=True)
-    gameStatus = db.Column(db.Integer, db.ForeignKey('cs.GameStatus.id'))
-    playerCount = db.Column(db.Integer)
-    gameCode = db.Column(db.String)
-    currentPlayer = db.Column(db.Integer, db.ForeignKey('cs.ActivePlayers.id'))
-
-# PlayerOrder Table
-class PlayerOrder(db.Model):
-    __tablename__ = 'PlayerOrder'
-    __table_args__ = {'schema': 'cs'}
-
-    id = db.Column(db.Integer, primary_key=True)
-    gameId = db.Column(db.Integer, db.ForeignKey('cs.ActiveGames.id'))
-    playerId = db.Column(db.Integer, db.ForeignKey('cs.ActivePlayers.id'))
-    turn = db.Column(db.Integer)
-
-
-# Characters Table
-class Characters(db.Model):
-    __tablename__ = 'Characters'
-    __table_args__ = {'schema': 'cs'}
-
-    id = db.Column(db.Integer, primary_key=True)
-    character = db.Column(db.String)
-
-
-# Locations Table
-class Locations(db.Model):
-    __tablename__ = 'Locations'
-    __table_args__ = {'schema': 'cs'}
-
-    id = db.Column(db.Integer, primary_key=True)
-    locationName = db.Column(db.String)
-
-
-# PlayerLocations Table
-class PlayerLocations(db.Model):
-    __tablename__ = 'PlayerLocations'
-    __table_args__ = {'schema': 'cs'}
-
-    gameId = db.Column(db.Integer, db.ForeignKey('cs.ActiveGames.id'), primary_key=True)
-    playerId = db.Column(db.Integer, db.ForeignKey('cs.ActivePlayers.id'), primary_key=True)
-    characterId = db.Column(db.Integer, db.ForeignKey('cs.Characters.id'))
-    locationId = db.Column(db.Integer, db.ForeignKey('cs.Locations.id'))
-
-
-# Rooms Table
-class Rooms(db.Model):
-    __tablename__ = 'Rooms'
-    __table_args__ = {'schema': 'cs'}
-
-    id = db.Column(db.Integer, primary_key=True)
-    roomName = db.Column(db.String)
-
-
-# Weapons Table
-class Weapons(db.Model):
-    __tablename__ = 'Weapons'
-    __table_args__ = {'schema': 'cs'}
-
-    id = db.Column(db.Integer, primary_key=True)
-    weaponName = db.Column(db.String)
-
-
-# Paths Table
-class Paths(db.Model):
-    __tablename__ = 'Paths'
-    __table_args__ = {'schema': 'cs'}
-
-    id = db.Column(db.Integer, primary_key=True)
-    locationId1 = db.Column(db.Integer, db.ForeignKey('cs.Locations.id'))
-    locationId2 = db.Column(db.Integer, db.ForeignKey('cs.Locations.id'))
-
-
-# Solutions Table
-class Solutions(db.Model):
-    __tablename__ = 'Solutions'
-    __table_args__ = {'schema': 'cs'}
-
-    id = db.Column(db.Integer, primary_key=True)
-    characterId = db.Column(db.Integer, db.ForeignKey('cs.Characters.id'))
-    gameId = db.Column(db.Integer, db.ForeignKey('cs.ActiveGames.id'))
-    roomId = db.Column(db.Integer, db.ForeignKey('cs.Rooms.id'))
-    weaponId = db.Column(db.Integer, db.ForeignKey('cs.Weapons.id'))
+    id = Column(Integer, primary_key=True)
+    status = Column(String(20), nullable=False)
 
 
 # Guesses Table
-class Guesses(db.Model):
+class Guesse(db.Model):
     __tablename__ = 'Guesses'
     __table_args__ = {'schema': 'cs'}
 
-    id = db.Column(db.Integer, primary_key=True)
-    characterId = db.Column(db.Integer, db.ForeignKey('cs.Characters.id'))
-    gameId = db.Column(db.Integer, db.ForeignKey('cs.ActiveGames.id'))
-    roomId = db.Column(db.Integer, db.ForeignKey('cs.Rooms.id'))
-    weaponId = db.Column(db.Integer, db.ForeignKey('cs.Weapons.id'))
-    playerId = db.Column(db.Integer, db.ForeignKey('cs.ActivePlayers.id'))
+    id = Column(Integer, primary_key=True)
+    characterId = Column(Integer, ForeignKey('cs.Characters.id'), nullable=False)
+    gameId = Column(Integer, ForeignKey('cs.Games.id'), nullable=False)
+    locationId = Column(Integer, ForeignKey('cs.Locations.id'), nullable=False)
+    weaponId = Column(Integer, ForeignKey('cs.Weapons.id'), nullable=False)
+    playerId = Column(Integer, ForeignKey('cs.Users.id'), nullable=False)
 
 
-# Player Status Table
+# Hands Table
+class Hand(db.Model):
+    # TODO: add class method to generate hands at the start of the game
+    # TODO: add a class method to return all cards in a users hand as dictionary
+
+    __tablename__ = 'Hands'
+    __table_args__ = {'schema': 'cs'}
+
+    id = Column(Integer, primary_key=True)
+    cardId = Column(Integer, ForeignKey('cs.Cards.id'), nullable=False)
+    playerInfo = Column(Integer, ForeignKey('cs.PlayerInfos.id'), nullable=False)
+
+
+# Locations Table
+class Location(db.Model):
+    __tablename__ = 'Locations'
+    __table_args__ = {'schema': 'cs'}
+
+    id = Column(Integer, primary_key=True)
+    locationName = Column(String(55), nullable=False)
+    isRoom = Column(Boolean, default=False, nullable=False)
+
+
+# Paths Table
+class Path(db.Model):
+    __tablename__ = 'Paths'
+    __table_args__ = {'schema': 'cs'}
+
+    id = Column(Integer, primary_key=True)
+    locationId1 = Column(Integer, ForeignKey('cs.Locations.id'), nullable=False)
+    locationId2 = Column(Integer, ForeignKey('cs.Locations.id'), nullable=False)
+    isSecret = Column(Boolean, default=False)
+
+
+# PlayerInfo
+class PlayerInfo(db.Model):
+
+    __tablename__ = 'PlayerInfos'
+    __table_args__ = {'schema': 'cs'}
+
+    id = Column(Integer, primary_key=True)
+    gameId = Column(Integer, ForeignKey('cs.Games.id'), nullable=False)
+    characterId = Column(Integer, ForeignKey('cs.Characters.id'), nullable=False)
+    locationId = Column(Integer, ForeignKey('cs.Locations.id'), nullable=False)
+    isEliminated = Column(Boolean, default=False, nullable=False)
+    playerId = Column(Integer, ForeignKey('cs.Users.id'), nullable=False)
+
+
+# PlayerOrder Table
+class PlayerOrder(db.Model):
+    #TODO: update default of turn to be an increment of the current max in the playerOrder table or just remove?
+
+    __tablename__ = 'PlayerOrder'
+    __table_args__ = {'schema': 'cs'}
+
+    id = Column(Integer, primary_key=True)
+    gameId = Column(Integer, ForeignKey('cs.Games.id'), nullable=False)
+    playerId = Column(Integer, ForeignKey('cs.Users.id'), nullable=False)
+    turn = Column(Integer, default=-1, nullable=False)
+    activeTurn = Column(Boolean, default=False, nullable=False)
+
+
+# PlayerStatus table
 class PlayerStatus(db.Model):
+
     __tablename__ = 'PlayerStatus'
     __table_args__ = {'schema': 'cs'}
 
-    id = db.Column(db.Integer, primary_key=True)
-    gameId = db.Column(db.Integer, db.ForeignKey('cs.ActiveGames.id'))
-    playerId = db.Column(db.Integer, db.ForeignKey('cs.ActivePlayers.id'))
-    characterId = db.Column(db.Integer, db.ForeignKey('cs.Characters.id'))
-    locationId = db.Column(db.Integer, db.ForeignKey('cs.Locations.id'))
+    id = Column(Integer, primary_key=True)
+    status = Column(String(20), nullable=False)
 
 
-# Card Table
-class Card(db.Model):
-    __tablename__ = 'Card'
+# StartLocations table - for initial character starting location
+class StartLocation():
+    # TODO: add a class method to output locations as dict 
+    #   (json format) 
+    #   (maybe input is character names?)
+
+    __tablename__ = 'StartLocations'
     __table_args__ = {'schema': 'cs'}
 
-    id = db.Column(db.Integer, primary_key=True)
-    weaponId = db.Column(db.Integer, db.ForeignKey('cs.Weapons.id'))
-    roomId = db.Column(db.Integer, db.ForeignKey('cs.Rooms.id'))
-    characterId = db.Column(db.Integer, db.ForeignKey('cs.Characters.id'))
+    characterId = Column(Integer, ForeignKey('cs.Characters.id'), nullable=False)
+    locationId = Column(Integer, ForeignKey('cs.Locations.id'), nullable=False)
 
 
-# Hand Table
-class Hand(db.Model):
-    __tablename__ = 'Hand'
+# Solutions Table
+class Solution(db.Model):
+    # make gameId unique
+
+    __tablename__ = 'Solutions'
     __table_args__ = {'schema': 'cs'}
 
-    id = db.Column(db.Integer, primary_key=True)
-    playerStatusId = db.Column(db.Integer, db.ForeignKey('cs.PlayerStatus.id'))
-    cardId = db.Column(db.Integer, db.ForeignKey('cs.Card.id'))
+    id = Column(Integer, primary_key=True)
+    characterId = Column(Integer, ForeignKey('cs.Characters.id'), nullable=False)
+    gameId = Column(Integer, ForeignKey('cs.Games.id'), nullable=False)
+    locationId = Column(Integer, ForeignKey('cs.Locations.id'), nullable=False)
+    weaponId = Column(Integer, ForeignKey('cs.Weapons.id'), nullable=False)
+
+# Users table
+class User(db.Model):
+    # TODO: add default function for populated a unique 4 character playerCode
+    # TODO: add default value to playerStatus (set to inactive)
+    # TODO: make playerCode not nullable
+    # TODO: add default to playerStatus
+    # TODO: make player status not nullable
+    # TODO: make playerCode, Username unique
+
+    __tablename__ = 'Users'
+    __table_args__ = {'schema': 'cs'}
+
+    id = Column(Integer, primary_key=True)
+    sessionInfo = Column(String(20))
+    playerCode = Column(String(6))
+    username = Column(String, nullable=False)
+    playerStatus = Column(Integer, ForeignKey('cs.PlayerStatus.id'))
+
+
+# Weapons Table
+class Weapon(db.Model):
+    __tablename__ = 'Weapons'
+    __table_args__ = {'schema': 'cs'}
+
+    id = Column(Integer, primary_key=True)
+    weaponName = Column(String(20))
+
+
+class WeaponLocation(db.Model):
+    # TODO: Add class method to populate initial locations per game
+    # TODO: Add class method to output the weapon locations as a dictionary (formatted for a json object)
+
+    __tablename__ = 'WeaponLocations'
+    __table_args__ = {'schema': 'cs'}
+
+    id = Column(Integer, primary_key=True)
+    locationId = Column(Integer, ForeignKey('cs.Locations.id'), nullable=False)
+    gameId = Column(Integer, ForeignKey('cs.Games.id'), nullable=False)
+    weapondId = Column(Integer, ForeignKey('cs.Weapons.id'), nullable=False)
+
+
+# Winners table
+class Winner(db.Model):
+    # TODO: make gameId unique
+
+    __tablename__ = 'Winners'
+    __table_args__ = {'schema': 'cs'}
+
+    id = Column(Integer, primary_key=True)
+    playerId = Column(Integer, ForeignKey('cs.Users.id'), nullable=False)
+    gameId = Column(Integer, ForeignKey('cs.Games.id'), nullable=False)
 

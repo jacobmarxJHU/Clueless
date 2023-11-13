f9e95c938900a9a5a5218fa58764483e44ba3a20
jacobmarxJHU <jmarx14@jh.edu>
2023-11-12 23:49:00 -0500

    added generate game state method to utility.py

--- flask_server/app/utility.py
+++ flask_server/app/utility.py
@@ -1,31 +1,12 @@
 from app import db
-from .models import PlayerInfo, Character, Weapon, Location, WeaponLocation, Game
+from .models import PlayerInfo, WeaponLocation
 
 
-def genLocationObject(gameCode: str) -> dict:
-
-    # get game
-    game = Game.query.filter_by(gameCode=gameCode).first()
-
-
-    # TODO: change this to a join statement
-    # get weapon locations -> create dictionary of weapons:locations <- both full text
-    weaponLocs = WeaponLocation.query.filter_by(gameId=game.id).all()
-    weaponDict = {}
-
-    for wl in weaponLocs:
-        wep = Weapon.query.filter_by(id=wl.id).first()
-        loc = Location.query.filter_by(id=wl.id).first()
-        weaponDict[wep.weaponName] = loc.locationName
-
-    print(weaponDict)
-
-    # get user locations -> create dictionary of usernames:locations <- both full text
-    player = PlayerInfo.query.filter_by(gameId=game.id).all()
-    userDict = {}
-
-    for ul in player:
-        pass
+def generateGameState(gamecode):
+    playerState = PlayerInfo.getGameState(gamecode)
+    weaponState = WeaponLocation.getWeaponState(gamecode)
+    
+    return {"userState": playerState, "weaponState": weaponState}
 
 
 # Utility function to commit changes to database as they're made

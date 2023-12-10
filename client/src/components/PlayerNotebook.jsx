9397b6d5790b1d070ab5d123c88664eb91bbba86
Nicolas Miller <njmiller1208@gmail.com>
2023-12-10 17:37:56 -0500

    Tentative solve for double clicks on "End Turn". Added an actionType parameter to handleActionSubmit to handle asynchronous React operations.

--- client/src/components/PlayerNotebook.jsx
+++ client/src/components/PlayerNotebook.jsx
@@ -13,10 +13,10 @@ import {
 } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
-  header: {
-    textAlign: 'center',
-    padding: theme.spacing(2),
-  },
+  // header: {
+  //   textAlign: 'center',
+  //   padding: theme.spacing(2),
+  // },
   tableCell: {
     borderBottom: 'none',
     fontWeight: 'bold',
